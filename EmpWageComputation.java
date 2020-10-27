9c1ccebf4391e261de0cede4c6e604f9174e9c68
301prateek <prateek.patil301@gmail.com>
2020-10-27 20:32:13 +0530

    Added: Part Time Wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -6,13 +6,14 @@ public class EmpWageComputation{
 		System.out.println("Welcome to Employee Wage Computation");
 
 		Random rd = new Random();
-		int a = rd.nextInt(2);
+		int a = rd.nextInt(3);
 
 		System.out.println(a);
 
 		int emp_present = 1;
 		int emp_absent  = 0;
 		int wage_per_hr =20;
+		int emp_part_time = 2;
 
 		if(a == emp_present){
 			System.out.println("Employee Present");
@@ -21,12 +22,17 @@ public class EmpWageComputation{
 			System.out.println("Daily salary: "+salary);
 
 		}
-		else{
-			System.out.println("Employee Absent");
-			int time = 0;
+		else if(a == emp_part_time){
+			System.out.println("Employee Present Part time");
+			int time = 4;
 			int salary = wage_per_hr*time ;
 			System.out.println("Daily salary: "+salary);
 		}
-
+		else{
+                        System.out.println("Employee Absent");
+                        int time = 0;
+                        int salary = wage_per_hr*time ;
+                        System.out.println("Daily salary: "+salary);
+		}
 	}
 }
