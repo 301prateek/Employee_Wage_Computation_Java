05becc3e107382ef6488c24f3e36467ab7576a58
301prateek <prateek.patil301@gmail.com>
2020-10-27 18:44:56 +0530

    Added: Attendance Check

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,5 +4,21 @@ public class EmpWageComputation{
 
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation");
+
+		Random rd = new Random();
+		int a = rd.nextInt(2);
+
+		System.out.println(a);
+
+		int emp_present = 1;
+		int emp_absent  = 0;
+
+		if(a == emp_present){
+			System.out.println("Employee Present");
+		}
+		else{
+			System.out.println("Employee Absent");
+
+		}
 	}
 }
