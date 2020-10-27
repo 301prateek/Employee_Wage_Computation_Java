d90d6b8d06caf8c9b2896c5653ec1a682ecb9448
301prateek <prateek.patil301@gmail.com>
2020-10-27 23:03:28 +0530

    Added: Case Statement

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -34,5 +34,31 @@ public class EmpWageComputation{
                         int salary = wage_per_hr*time ;
                         System.out.println("Daily salary: "+salary);
 		}
+
+		System.out.println("=========Using Case Statement============");
+
+		switch(a){
+
+		case 1:
+			System.out.println("Employee Present");
+			int t1 =8;
+			int sal1 =wage_per_hr*t1;
+			System.out.println("Salary "+sal1);
+			break;
+		case 2:
+                        System.out.println("Employee Present Part time");
+                        int t2 =4;
+                        int sal2 =wage_per_hr*t2;
+                        System.out.println("Salary "+sal2);
+			break;
+		case 0:
+                        System.out.println("Employee Absent");
+                        int t3 =0;
+                        int sal3 =wage_per_hr*t3;
+                        System.out.println("Salary "+sal3);
+			break;
+		default:
+			System.out.println("Wrong input");
+		}
 	}
 }
